0a2e51d818e4bac571b430812c141d5079e1e45d
FrancescoPalazzo97 <francescopalazzo09@gmail.com>
2025-04-03 16:19:43 +0200

    [DONE] Esercizio fatto + prompt che chiede all'utente il numero di mail da generare

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
-const n = 10; // Definisco una variabile che conta quanto email gnerare
+// Funtions
+const createListItem = (value) => {
+    let li = document.createElement(`li`); // Creo un elemento li 
+    li.innerHTML = email; // Inserisco il valore ricevuto prima dalla chiamata AJAX dentro il tag li
+    document.querySelector(`ul`).appendChild(li); // Lo inserisco dentro la lista
+};
 
-for (i = 0; i < n; i++) { // Creo un ciclo che mi ripete la chiamata per n volte
-    axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(resp => { // Faccio la chimata AJAX
-        email = resp.data.response // Metto il valore dentro la variabile email
-        let li = document.createElement(`li`); // Creo un elemento li 
-        li.innerHTML = email; // Inserisco il valore ricevuto prima dalla chiamata AJAX dentro il tag li
-        document.querySelector(`ul`).appendChild(li); // Lo inserisco dentro la lista
-    })
+const generateRandomEmail = (x) => {
+    for (i = 0; i < x; i++) { // Creo un ciclo che mi ripete la chiamata per x volte, dove x è il numero inserito dall'utente
+        axios.get(`https://flynn.boolean.careers/exercises/api/random/mail`).then(resp => { // Faccio la chimata AJAX
+            email = resp.data.response // Metto il valore dentro la variabile email
+            createListItem(email) // Richiamo la funzione
+        })
+    }
 }
+
+const n = parseInt(prompt(`Quante email vuoi generare?`)); // Definisco una variabile che conta quanto email generare
+
+generateRandomEmail(n); // Richiamo la funzione che effettua la chiamata AJAX passandogli il valore inserito dall'utente
+
+
